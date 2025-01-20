63e495d83201341ed67bc9a6c0b7900eeb882b42
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2025-01-20 12:04:19 +0200

    initial commit

---
+++ parking_app/README.md
@@ -0,0 +1,9 @@
+# SWORD Parking App
+
+## Initial Readme
+
+### Installation
+
+- clone the repo
+- npm install
+- npm run dev
---
+++ parking_app/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        My Parking App
+      </div>
+    </>
+  )
+}
+
+export default App
