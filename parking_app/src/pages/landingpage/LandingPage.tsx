a0538900b85533c72e13676c1c300d774705190d
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2025-01-21 01:34:57 +0200

    more application cards

--- parking_app/src/pages/landingpage/LandingPage.tsx
+++ parking_app/src/pages/landingpage/LandingPage.tsx
@@ -5,7 +5,22 @@ const LandingPage = () => {
     return (
         <div>
             <h1>Landing Page</h1>
-            <ApplicationCard title="Application" content="Go to the application" relativeUrl="/application" bgColour="lightgrey" />
+            <ApplicationCard title="Application" 
+                content="Go to the application" 
+                relativeUrl="/application" 
+                bgColour="lightgrey" />
+            <ApplicationCard title="Parking Complaints" 
+                content="Register a parking complaint against a car already parked at your designated spot" 
+                relativeUrl="/parking-complaints" 
+                bgColour="#98C5C2" />
+            <ApplicationCard title="Parking Allocation"
+                content="Allocate a parking spot to a car" 
+                relativeUrl="/parking-allocation" 
+                bgColour="#D5C4E7" />
+            <ApplicationCard title="My Car"
+                content="Register your car in the system" 
+                relativeUrl="/my-car" 
+                bgColour="#D5E6F6" />                
         </div>
     );
 
